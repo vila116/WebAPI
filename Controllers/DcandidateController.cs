07699450d64eabd676a68d9672ef524617bf0163
Hp <Hp@DESKTOP-7M71V1A>
2025-08-07 22:08:47 +0530

    Add project files.

---
+++ Controllers/DcandidateController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DcandidateController : ControllerBase
+    {
+        private readonly DonationDBContext _context;
+        public DcandidateController(DonationDBContext context)
+        {
+            _context = context;
+        }
+        // Get : api/Dcandidate/5
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<DCandidate>>> GetDcandidates()
+        {
+
+            return await _context.dCandidates.ToListAsync();
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<DCandidate>> GetDcandidate(int id)
+        {
+            var _dcandidate = await _context.dCandidates.FindAsync(id);
+            if (_dcandidate==null)
+                return NotFound();
+            return _dcandidate;
+
+        }
+
+    }
+}
---
+++ Models/DonationDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebAPI.Models
+{
+    public class DonationDBContext: DbContext
+    {
+        public DonationDBContext(DbContextOptions<DonationDBContext> options):base(options) 
+        {
+            
+        }
+        public DbSet<DCandidate> dCandidates { get; set; }
+    }
+}
